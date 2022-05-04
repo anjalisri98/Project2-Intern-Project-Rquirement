5aa29f004146e0c709434b1e42bcf641da531bfc
Anjali Srivastava <anjalisriv023@gmail.com>
2022-05-04 15:55:23 +0530

    done

--- route.js
+++ route.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const controller = require('../controller/controller')
+//const middleware=require('../middleware/middleware')
+const router = express.Router();
+
+
+
+
+
+
+module.exports = router;
